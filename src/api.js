145083b2a31d358168d03673193654fd5dd8565e
KieceDonc <valentinverst.developer@gmail.com>
2020-12-31 17:20:35 +0100

    updating domain / port

--- src/api.js
+++ src/api.js
@@ -4,10 +4,11 @@ const https = require('https')
 const database = require('./database')
 const twitchAPI = require('./twitch-api');
 
-const domain="uptextv.com"
-const port = 3000; 
+const domain="api.uptextv.com"
+const port = 443; 
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/'+domain+'/privkey.pem', 'utf8'); // https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
+// https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/'+domain+'/privkey.pem', 'utf8'); 
 const certificate = fs.readFileSync('/etc/letsencrypt/live/'+domain+'/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/'+domain+'/chain.pem', 'utf8');
 
